bba5e01aa57cdc9820c0cd921b083787e1e6aa42
jonathan440 <32670848+jonathan440@users.noreply.github.com>
2018-09-06 15:19:21 +0200

    Add files via upload

--- Grapher/src/parser.java
+++ Grapher/src/parser.java
@@ -29,30 +29,69 @@ public class parser{
 	
 	
 	public static void parsing(String expression){
-		for(int i = 0; i < expression.length();i++) {
-			if(expression.charAt(i) == '+') {
-				System.out.println("Noeud Binaire");
-				System.out.println("Racine :" + expression.charAt(i));
-				System.out.println("Fg : " + expression.charAt(i -1));
-				System.out.println("Fd : " + expression.charAt(i + 1));
-				
-				
-			}
-			
-			if(expression.charAt(i)== '-') {
-				System.out.println("Noeud Binaire");
-			}
-			
-			if(expression.charAt(i) == '*') {
-				System.out.println("Noeud Binaire");
-			}
-			if(expression.charAt(i) == '/') {
-				System.out.println("Noeud Binaire");
+		char operators[] = {'/', '*', '-', '+'};
+		for(char operator : operators) {
+			for(int i = 0; i < expression.length();i++) {
+				if(expression.charAt(i) == operator) {
+					System.out.println("FG : "+ getPreviousNumber(expression, i));
+					System.out.println("FD :"+ getNextNumber(expression, i));
+					double fg = getPreviousNumber(expression, i);
+					double fd = getNextNumber(expression, i);
+					
+					if(operator  == '/') {
+						Division D = new Division(new Constant(fg), new Constant(fd));
+						
+					}
+					
+					if(operator == '*') {
+						Multiplication m = new Multiplication(new Constant(fg), new Constant(fd));
+					}
+					
+					if(operator == '+') {
+						addition a = new addition(new Constant(fg),new Constant(fd));
+						System.out.println(""+ a.execute());
+									
+					}
+					
+					if(operator == '-') {
+						Soustraction s = new Soustraction(new Constant(fg), new Constant(fd));
+					}
+				}
 			}
 		}
+		
+	}
+	
+	public Noeud buildArbre(Noeud fg, Noeud fd) {
+		NoeudBinaire a = new NoeudBinaire(fg,fd);
+		return a;
+		
 	}
 	
 	
+	private static double getPreviousNumber(String expression, int index) 
+	{
+		String number = "";
+		index--;
+		while(index >= 0 && (Character.isDigit(expression.charAt(index)) || expression.charAt(index) == '.')) {
+			number = expression.charAt(index) + number;
+			index--;
+		}
+		return Double.parseDouble(number);
+		
+	}
+	
+	
+	private static double getNextNumber(String expression, int index) 
+	{
+		String number = "";
+		index++;
+		while(index < expression.length() &&(Character.isDigit(expression.charAt(index)) || expression.charAt(index) == '.')) {
+			number = number +  expression.charAt(index);
+			index++;
+		}
+		return Double.parseDouble(number);
+	}
 	
 
 	public static void main(String[] args) {
@@ -62,7 +101,6 @@ public class parser{
 		String expression = lectureClavier();
 		
 		System.out.println("Expression saisie : "+ expression);
-		
 		parsing(expression);
 		
 		
@@ -70,7 +108,7 @@ public class parser{
 
 	
 
-
+	
 
 	
 }
